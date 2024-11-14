6adb3db7267cabd2d62b74361562a5d5ab1e51be
Ashish Laskar <ashish.laskar@uniqgridcloud.com>
2024-11-14 10:31:55 +0530

    new teckpix123

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('*', (req, res) => {
 res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 // if not in production use the port 5000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // const URL = 192.168.2.1
 console.log('server started on port:',PORT);
 app.listen(PORT);
