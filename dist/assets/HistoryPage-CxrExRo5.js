ced19e9666dda3fdf82069d1b8bfbed0e820c640
Ashish <ashish.laskar@uniqgridcloud.com>
2026-08-20 16:24:07 +0530

    checksheet gpp

--- dist/assets/HistoryPage-CxrExRo5.js
+++
@@ -1,1 +0,0 @@
-import{j as o,aF as r}from"./index-DAkBlfIF.js";import{a as s}from"./fields-DVSu7fr5.js";import{H as t}from"./HistoryTab-O2TdpQA2.js";import"./index-Cy9_GSXF.js";const p=()=>o.jsx(r,{sx:{backgroundColor:s.bg,minHeight:"100%",padding:"18px 20px"},children:o.jsx(t,{})});export{p as default};
